ae635a4ff044cf7bdc1e74b8ea74f266ea7508df
Frederik Foss Nielsen <frederik2812@gmail.com>
2017-09-19 08:34:21 +0000

    Hej

---
+++ IntroToNode/DemoApp/app.js
@@ -0,0 +1,1 @@
+console.log("FROM APP.JS");
--- IntroToNode/grader.js
+++ IntroToNode/grader.js
@@ -1,14 +1,16 @@
 
 function average(numbers) {
-  var wholeNum = 0;
+  var total = 0;
   for (var i = 0; i < numbers.length; i++) {
-    wholeNum += numbers[i];
+    total += numbers[i];
   }
-  wholeNum = round(wholeNum / numbers.length);
+  return Math.round(total / numbers.length);
 }
 
+console.log("Average score for environment science")
 var scores = [90, 98, 89, 100, 100, 86, 94];
-average(scores);
+console.log(average(scores));
 
+console.log("Average score for organic chemistry")
 var scores2 = [40, 65, 77, 82, 80, 54, 73, 63, 95, 49];
-average(scores2);
+console.log(average(scores2));
