5210d4a721a47d3f7735bc6235281aef3aaf679e
Frederik Foss Nielsen <frederik2812@gmail.com>
2017-09-19 09:58:03 +0000

    heh

--- IntroToNPM/FakerApp/app.js
+++
@@ -1,3 +0,0 @@
-var faker = require('faker')
-
-console.log(faker());
---
+++ IntroToNPM/MyShop/listProducts.js
@@ -0,0 +1,10 @@
+function items(number) {
+  var faker = require('faker')
+  console.log("==================================")
+  console.log("W E L C O M E  T O  M Y  S H O P !")
+  console.log("==================================")
+  for (var i = 0; i <= number; i++) {
+      console.log(faker.commerce.productName() + " - $" + faker.commerce.price());
+  }
+}
+items(10);
