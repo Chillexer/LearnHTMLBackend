1ed55ca8e78da8bb5f5b7db45786c526b5468b5a
Frederik Nielsen <frederikfossnielsen@hotmail.com>
2017-09-20 20:09:06 +0200

    Auto

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -3,26 +3,38 @@ var app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+if (/^win/.test(process.platform)) {
+	app.set('views', __dirname + '/views');
+	app.use(express.static(__dirname + '/public'));
+}
 
-app.get("/", function(req, res){
-    res.render("home");
+app.get("/", function (req, res) {
+	res.render("home");
 });
 
-app.get("/fallinlovewith/:thing", function(req, res){
-    var thing = req.params.thing;
-    res.render("love", {thingVar: thing});
+app.get("/fallinlovewith/:thing", function (req, res) {
+	var thing = req.params.thing;
+	res.render("love", { thingVar: thing });
 });
 
-app.get("/posts", function(req, res){
-    var posts = [
-        {title: "Post 1", author: "Susy"},
-        {title: "My adorable pet bunny", author: "Charlie"},
-        {title: "Hello world", author: "Frederik"}
-        ];
-        
-        res.render("posts", {posts: posts});
+app.get("/posts", function (req, res) {
+	var posts = [
+		{ title: "Post 1", author: "Susy" },
+		{ title: "My adorable pet bunny", author: "Charlie" },
+		{ title: "Hello world", author: "Frederik" }
+	];
+
+	res.render("posts", { posts: posts });
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Server is started!!!");
+app.listen(3000, function () {
+	console.log("Server is started!!!");
 });
+
+
+
+
+
+
+
+
