10fd30d30c874dd7629fbfd838d87dccb67f8110
Frederik Foss Nielsen <frederik2812@gmail.com>
2017-09-25 14:53:57 +0200

    update
    up

--- YelpCamp/v2/app.js
+++ YelpCamp/v2/app.js
@@ -1,44 +1,82 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express     = require("express"), 
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose");
 
+mongoose.Promise = global.Promise;
+
+mongoose.connect("mongodb://192.168.0.111:27017/yelp_camp", {useMongoClient: true});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
+})
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//  { 
+//    name: "Granite Hill", 
+//    image: "https://farm9.staticflickr.com/8225/8524305204_43934a319d.jpg",
+//    description: "This is at huge granite hill, no bathrooms. No water. Beutiful granite!"
+//  
+//  },  function(err, campground){
+//    if (err) {
+//      console.log(err);
+//    } else {
+//      console.log("NEWLY CREATED CAMPGROUND: ");
+//      console.log(campground);
+//    }
+//  });
+
 
 app.get("/", function (req, res) {
   res.render("landing");
 });
 
-var campgrounds = [
-  { name: "Salmon Creek", image: "https://farm9.staticflickr.com/8225/8524305204_43934a319d.jpg" },
-  { name: "Salmon Creek", image: "https://farm9.staticflickr.com/8225/8524305204_43934a319d.jpg" },
-  { name: "Salmon Creek", image: "https://farm9.staticflickr.com/8225/8524305204_43934a319d.jpg" },
-  { name: "Salmon Creek", image: "https://farm9.staticflickr.com/8225/8524305204_43934a319d.jpg" },
-  { name: "Salmon Creek", image: "https://farm9.staticflickr.com/8225/8524305204_43934a319d.jpg" },
-  { name: "Salmon Creek", image: "https://farm9.staticflickr.com/8225/8524305204_43934a319d.jpg" },
-  { name: "Salmon Creek", image: "https://farm9.staticflickr.com/8225/8524305204_43934a319d.jpg" },
-  { name: "Salmon Creek", image: "https://farm9.staticflickr.com/8225/8524305204_43934a319d.jpg" },
-  { name: "Salmon Creek", image: "https://farm9.staticflickr.com/8225/8524305204_43934a319d.jpg" },
-  { name: "Salmon Creek", image: "https://farm9.staticflickr.com/8225/8524305204_43934a319d.jpg" },
-  { name: "Salmon Creek", image: "https://farm9.staticflickr.com/8225/8524305204_43934a319d.jpg" }
-];
-
 app.get("/campgrounds", function (req, res) {
-  res.render("campgrounds", { campgrounds: campgrounds });
+  Campground.find({}, function(err, allCampgrounds){
+    if (err) {
+      console.log(err);
+    } else{
+      res.render("Index", { campgrounds: allCampgrounds });
+    }
+  })
+  // res.render("campgrounds", { campgrounds: campgrounds });
 });
 
 app.post("/campgrounds", function (req, res) {
   var name = req.body.name;
   var image = req.body.image;
-  var newCampground = {name: name, image: image};
-  campgrounds.push(newCampground);
-
-  res.redirect("/campgrounds");
+  var desc = req.body.description;
+  var newCampground = {name: name, image: image, description: desc};
+  
+  Campground.create(newCampground, function (err, newluCreated) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
 });
 
 app.get("/campgrounds/new", function (req, res) {
   res.render("new");
+});
+
+app.get("/campgrounds/:id", function(req, res){
+  Campground.findById(req.params.id, function(err, foundCampground){
+    if(err){
+      console.log(err);
+    } else {
+      res.render("show", {campground: foundCampground});
+    }
+  })
 });
 
 app.listen(3000, function () {
