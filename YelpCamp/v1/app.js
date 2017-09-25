775ecec7468f39c29cc53e44b448808553209f60
Frederik Foss Nielsen <frederik2812@gmail.com>
2017-09-25 09:56:49 +0200

    update

--- YelpCamp/v1/app.js
+++ YelpCamp/v1/app.js
@@ -6,6 +6,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+
+
 app.get("/", function (req, res) {
   res.render("landing");
 });
